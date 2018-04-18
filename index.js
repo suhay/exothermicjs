944ed558860f7d748b6bc8441eb9c9bbe263754f
suhay <matthew.suhay@gmail.com>
2018-04-17 21:08:57 -0400

    Setting dev export to dist folder instead of lib

--- index.js
+++ index.js
@@ -3,5 +3,5 @@
 if (process.env.NODE_ENV === 'production') {
   module.exports = require('./dist/reacty.min.js');
 } else {
-  module.exports = require('./lib/reacty.js');
+  module.exports = require('./dist/reacty.js');
 }
