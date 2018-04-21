1bf286538cf8aaa8dcf6a32977460429e0b4d46d
suhay <matthew.suhay@gmail.com>
2018-04-20 21:22:18 -0400

    Updating test

--- tests/reacty.test.js
+++ tests/reacty.test.js
@@ -4,5 +4,5 @@ var path = require('path');
 var pages =  path.resolve(__dirname, '../demo/dist/pages');
 
 test('demo site renders', () => {
-  expect(Reacty.build('/', pages)).toContain('Where you can find me');
+  expect(Reacty.build('/', pages)).toContain('Where - End of file!!');
 });
