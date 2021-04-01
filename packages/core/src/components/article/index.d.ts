d0023d1512248f45ba245d724b83f0688a2281bd
Matt Suhay <matthew.suhay@gmail.com>
2021-04-01 11:19:45 -0500

    Chagning semvar

--- packages/core/src/components/article/index.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="react" />
-import { PageFragment } from '../../types';
-export declare const Article: ({ id, title, items, class: classes, content, }: PageFragment) => JSX.Element;
